ba1a5735d92bd59f1de653992399425bb07bb2c9
shb1204st <shb1204st@gmail.com>
2021-07-20 22:03:06 +0900

    バリデーション作成
    database.ymlのencoding: utf8mb4をencoding: utf8へ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true
+  validates :last_name,          presence: true
+  validates :kana_first_name,    presence: true
+  validates :kana_last_name,     presence: true
+  validates :birthday,           presence: true
+  
 end
