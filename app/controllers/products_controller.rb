9073216acce971d0d24d51259b5259b58fa94cab
shb1204st <shb1204st@gmail.com>
2021-07-23 22:02:00 +0900

    画像投稿機能
    ※Gemfileにてactive_hashのgemもインストールしている
    ※カラム名変更plase→place

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,16 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.create(product_params)
+  end
+
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image).merge(user_id: current_user_id)
   end
 
 end
