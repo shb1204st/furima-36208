255a528acb4f32d0b8c32391a999a735f838cbb0
shb1204st <shb1204st@gmail.com>
2021-07-23 21:35:18 +0900

    商品出品のコントロールとアクション設定
    その他訂正事項あり

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,11 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+  end
+
 end
--- db/migrate/20210723080454_create_products.rb
+++ db/migrate/20210723080454_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.integer    :category_id,        null: false
       t.integer    :product_status_id,  null: false
       t.integer    :cost_id,            null: false
-      t.integer    :seller_plase_id,    null: false
+      t.integer    :seller_place_id,    null: false
       t.integer    :need_days_id,       null: false
       t.integer    :price,              null: false
       t.references :user,               null: false, foreign_key: true
