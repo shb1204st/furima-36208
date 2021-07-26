85a3f85e2c472863d430eff7a2c0af9b76b32e0c
shb1204st <shb1204st@gmail.com>
2021-07-26 21:58:25 +0900

    addressモデルとテーブル作成

---
+++ db/migrate/20210726124817_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string     :post_code,       null: false
+      t.integer    :seller_place_id, null: false
+      t.string     :city,            null: false
+      t.string     :address,         null: false
+      t.string     :build_name
+      t.string     :phone_num
+      t.references :buy,             null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
