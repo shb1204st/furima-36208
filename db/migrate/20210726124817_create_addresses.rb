63259e9125abd182888e4fc826db8c843ed91600
shb1204st <shb1204st@gmail.com>
2021-07-28 21:53:03 +0900

    商品購入　pull request 指摘事項

--- db/migrate/20210726124817_create_addresses.rb
+++ db/migrate/20210726124817_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :city,            null: false
       t.string     :address,         null: false
       t.string     :build_name
-      t.string     :phone_num
+      t.string     :phone_num,       null:false
       t.references :buy,             null: false, foreign_key: true
       t.timestamps
     end
