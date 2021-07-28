088878d2f9ab4c54c92f99158ac3a713b93560cc
shb1204st <shb1204st@gmail.com>
2021-07-28 21:04:01 +0900

    商品購入　pull reqest7 バリデーションのリファクタリング

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -2,13 +2,13 @@ class BuyAddress
   include ActiveModel::Model
   attr_accessor :post_code, :seller_place_id, :city, :address, :build_name, :phone_num, :user_id, :product_id, :token
 
-  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  
   validates :seller_place_id, numericality: {other_than: 0, message: "can't be blank"}
   
   with_options presence: true do
-    validates :user_id, :product_id, :city, :address
+    validates :user_id, :product_id, :city, :address, :token
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_num, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
-    validates :token
   end
   
 
