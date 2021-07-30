04f554bab1a18103003ba592858efb3ae6ca0023
shb1204st <shb1204st@gmail.com>
2021-07-31 07:50:32 +0900

    検索機能　ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :products do
+    collection do  
+      get "search"
+    end
     resources :buys, only: [:index, :create]
   end
 end
