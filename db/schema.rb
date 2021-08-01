808d8dda0e84f8f1d59a27ae4a0d769e22a9af1c
shb1204st <shb1204st@gmail.com>
2021-08-01 19:52:00 +0900

    コメント機能　コメントモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_124817) do
+ActiveRecord::Schema.define(version: 2021_08_01_073930) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_124817) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "message"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
