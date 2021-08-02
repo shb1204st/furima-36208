ad3ac6ce1f950bf959f802a8d54f18e49e806206
shb1204st <shb1204st@gmail.com>
2021-08-02 12:39:26 +0900

    コメント機能　commentテーブル作成
    user_idとproduct_idとでアソシエーションを組む

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_01_073930) do
+ActiveRecord::Schema.define(version: 2021_08_01_105001) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2021_08_01_073930) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["product_id"], name: "index_buys_on_product_id"
     t.index ["user_id"], name: "index_buys_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
